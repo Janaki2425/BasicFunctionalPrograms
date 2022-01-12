dfc6c621fc1ed8ee3e1e7b9cc812117f93270249
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 01:21:56 +0530

    adding a file

---
+++ Leapyr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicFunctionalPrograms
+{
+    class LeapYr
+    {
+        public static void checkLeapYear()
+        {
+            int check_Leapyear;
+            Console.WriteLine("Check whether a given year is leap year or not:");
+            Console.WriteLine("Input an year : ");
+            check_Leapyear = Convert.ToInt32(Console.ReadLine());
+
+            if ((check_Leapyear % 400) == 0)
+                Console.WriteLine("{0} is a leap year.", check_Leapyear);
+            else if ((check_Leapyear % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.", check_Leapyear);
+            else if ((check_Leapyear % 4) == 0)
+                Console.WriteLine("{0} is a leap year.", check_Leapyear);
+            else
+                Console.WriteLine("{0} is not a leap year.", check_Leapyear);
+        }
+    }
+}
