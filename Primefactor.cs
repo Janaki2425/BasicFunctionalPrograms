748ef70f09ae957c79849419b43d2c3a1a62f75b
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 01:24:52 +0530

    adding a file

---
+++ Primefactor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionalPrograms
+{
+	internal class Primefactor
+	{
+		public static void Prime()
+		{
+			int n, i, p, k;
+
+			Console.Write("Enter a number:");
+			n = Convert.ToInt32(Console.ReadLine());
+
+			for (i = 2; i <= n; i++)
+			{
+				k = 2;
+				p = 1;
+				while (k < i)
+				{
+					if (i % k == 0)
+					{
+						p = 0;
+						break;
+					}
+					k++;
+				}
+				if (p == 1)
+				{
+					Console.WriteLine("Number is prime:" + i);
+				}
+			}
+		}
+	}
+
