34b22333c339d13e70ca02603a6094f5f94552d0
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 01:22:53 +0530

    adding a file

---
+++ OddEven.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionalPrograms
+{
+    internal class OddEven
+    {
+        public static void CheckEvenOddNumber()
+        {
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("number is even");
+            }
+            else
+            {
+                Console.WriteLine("number is odd");
+            }
+        }
+    }
+}
+    
