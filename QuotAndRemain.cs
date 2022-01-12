03159dc493f6fcf0fd6047b896d6a8ad2d5d5e78
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 01:26:24 +0530

    adding a file

---
+++ QuotAndRemain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionalPrograms
+{
+    internal class QuotAndRemain
+    {
+        public static void QuotientAndRemainder()
+        {
+            Console.WriteLine("enter the value of dividend and divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            double quotient = dividend / divisor;
+            double reminder = dividend % divisor;
+            Console.WriteLine("Quotient=" + quotient + "\n Reminder=" + reminder);
+        }
+    }
+}
+
