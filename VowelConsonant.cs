a6d307e8fdec2bf0ab6c8a599d8f872292829844
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 01:29:00 +0530

    adding a file

---
+++ VowelConsonant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+   
+namespace BasicFunctionalPrograms
+{
+    internal class VowelConsonant
+    { 
+        public static void UserInput()
+       {
+
+        Console.WriteLine("enter any alphabet");
+          char c = Convert.ToChar(Console.ReadLine());
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+            {
+                Console.WriteLine("alphabet is vowel");
+            }
+            else if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            {
+                Console.WriteLine("alphabet is consonant");
+             }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+          }
+     }       
+}
