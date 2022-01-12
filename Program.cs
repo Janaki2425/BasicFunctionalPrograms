45709871bd09e4c5e8c6f6648036c7ca6b8e4819
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 01:15:32 +0530

    add a file

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BasicFunctionalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             Flipcoin Uc1 = new Flipcoin();
+             Flipcoin.FlipCoin();
+             LeapYr Uc2 = new LeapYr();
+             LeapYr.checkLeapYear();
+             Primefactor Uc3 = new Primefactor();
+             Primefactor.Prime();
+             Harmonic Uc4 = new Harmonic();
+             Harmonic.harmonicSeries();
+             OddEven Uc5= new OddEven();
+             OddEven.CheckEvenOddNumber();
+             Largestamongthree Uc6 = new Largestamongthree();
+             Largestamongthree.LargestAmongThreeNumbers();
+             QuotAndRemain Uc7 = new QuotAndRemain();
+             QuotAndRemain.QuotientAndRemainder();
+             Powerof2 UC8 = new Powerof2();
+             Powerof2.power();
+             Swapnos UC9 = new Swapnos();
+             Swapnos.SwapTheNumbers();
+             VowelConsonant uc10 = new VowelConsonant();
+             VowelConsonant.UserInput();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
